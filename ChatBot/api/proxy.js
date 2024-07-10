cbce6e42d3a71af0e82519e32c8cba700d1a5eec
Jefino <jefinojacob9488@gmail.com>
2024-07-10 17:20:00 +0530

    test proxy.js

--- ChatBot/api/proxy.js
+++ ChatBot/api/proxy.js
@@ -1,12 +1,23 @@
 import fetch from 'node-fetch';
 
 export default async (req, res) => {
-  const response = await fetch('http://ec2-3-95-219-188.compute-1.amazonaws.com:8000/chat', {
-    method: req.method,
-    headers: req.headers,
-    body: req.body,
-  });
+  try {
+    const response = await fetch('http://ec2-3-95-219-188.compute-1.amazonaws.com:8000/chat', {
+      method: req.method,
+      headers: req.headers,
+      body: req.body,
+    });
 
-  const data = await response.text();
-  res.status(response.status).send(data);
+    const contentType = response.headers.get('content-type');
+    let data;
+    if (contentType && contentType.includes('application/json')) {
+      data = await response.json();
+    } else {
+      data = await response.text();
+    }
+
+    res.status(response.status).send(data);
+  } catch (error) {
+    res.status(500).send('Error fetching data from backend server');
+  }
 };
